65224563a70162d38338e34d06bdfde4b1869d93
Abel Aman <136691061+bellapacx@users.noreply.github.com>
2024-06-01 17:16:58 +0300

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -17,7 +17,7 @@ const RecentProjects = () => {
                  <PinContainer title={title} href={link}>
                     <div className='relative flex  items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm;h-[40vh] h-[30vh] mb-10'>
                       <div className='w-full h-full overflow-hidden lg:rounded-3xl bg:[#13162d]'>
-                         <img src='/bg.png' alt='bg-img'/>
+                         <img src='bg.png' alt='bg-img'/>
                       </div>
                       <img 
                       src={img} 
