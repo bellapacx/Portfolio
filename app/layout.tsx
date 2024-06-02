8f45dac2a5f62fed8f3ff7f6ddd8e3f17010f97f
Abel Aman <136691061+bellapacx@users.noreply.github.com>
2024-06-02 12:21:25 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Abel's Portfolio",
   description: "Modern",
-  icons: "backlogo.svg"
+  icons: "/backlogo.svg"
 };
 
 export default function RootLayout({
